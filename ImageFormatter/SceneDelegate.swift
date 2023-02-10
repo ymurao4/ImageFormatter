5bfac86d527191fdeb2dcf0dcb4d47edf72ab9ea
村尾慶伸 <muraoyoshinobu@muraokeishins-Mac-Studio-5.local>
2023-02-10 10:03:56 +0900

    first commit

---
+++ ImageFormatter/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  ImageFormatter
+//
+//  Created by 村尾慶伸 on 2023/02/10.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
